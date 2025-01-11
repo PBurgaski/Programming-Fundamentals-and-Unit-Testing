866ef4192d679028dd2743efdb4297d4fa5353d5
Petar Burgaski <pvalev95@gmail.com>
2025-01-11 21:46:43 +0200

    Commit

---
+++ 02.KeyConcepts/05.DepositCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Input
+
+double depositAmount = double.Parse(Console.ReadLine());
+int countOfMonts = int.Parse(Console.ReadLine());
+double interestPercentPerYear = double.Parse(Console.ReadLine());
+
+// Calculate
+
+double yearIncome = depositAmount * interestPercentPerYear / 100;
+double monthIncome = yearIncome / 12;
+double finalAmount = depositAmount + (countOfMonts * monthIncome);
+
+// Output
+
+Console.WriteLine($"{finalAmount:F2}");
+
---
+++ 02.KeyConcepts/06.MandatoryLiterature/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Input
+
+int countOfPagesToRead = int.Parse(Console.ReadLine());
+int pagesReadPerOneHour = int.Parse(Console.ReadLine());
+int countOfDays = int.Parse(Console.ReadLine());
+
+// Calculations
+
+                  // целочислено деление без остатък
+int neededHours = countOfPagesToRead / pagesReadPerOneHour; 
+
+int readingHoursPerDay = neededHours / countOfDays;
+
+// Output
+
+Console.WriteLine(readingHoursPerDay);
