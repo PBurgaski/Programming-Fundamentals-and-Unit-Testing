78484c782a8f1b0410e40d9fffe5fa078a71498d
Petar Burgaski <pvalev95@gmail.com>
2025-01-11 21:35:43 +0200

    Add files via upload

---
+++ 01.DataTypesAndVariables/09.DaysToMinutes/Program.cs
@@ -0,0 +1,6 @@
+﻿
+int countOfDays = int.Parse(Console.ReadLine());
+
+int calculatedMinutes = countOfDays * 24 * 60;
+
+Console.WriteLine($"Minutes = {calculatedMinutes}");
---
+++ 01.DataTypesAndVariables/10.CalculateSpeed/Program.cs
@@ -0,0 +1,7 @@
+﻿n 
+double distance = double.Parse(Console.ReadLine());
+double time = double.Parse(Console.ReadLine());
+
+double speed = distance / time;
+
+Console.WriteLine($"{speed:F2}");
