b72e21c31d4f375dcaeada7580ee5b2ab2a18c87
Petar Burgaski <pvalev95@gmail.com>
2025-01-11 21:45:45 +0200

    Commit

---
+++ 02.KeyConcepts/02.FourOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Input
+
+double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine());
+
+// Calculations
+
+double additionResult = firstNumber + secondNumber;
+double subtractResult = firstNumber - secondNumber;
+double multiplicationResult = firstNumber * secondNumber;
+double divideResult = firstNumber / secondNumber;
+
+// Output
+
+Console.WriteLine($"{firstNumber:F2} + {secondNumber:F2} = {additionResult:F2}");
+Console.WriteLine($"{firstNumber:F2} - {secondNumber:F2} = {subtractResult:F2}");
+Console.WriteLine($"{firstNumber:F2} * {secondNumber:F2} = {multiplicationResult:F2}");
+Console.WriteLine($"{firstNumber:F2} / {secondNumber:F2} = {divideResult:F2}");
