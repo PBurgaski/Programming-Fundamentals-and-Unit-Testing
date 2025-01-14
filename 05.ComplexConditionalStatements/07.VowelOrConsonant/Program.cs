511d1c413a7c8dd74af7df557961b82f327abf74
PBurgaski <pvalev95@gmail.com>
2025-01-14 22:44:55 +0200

    update

--- 05.ComplexConditionalStatements/07.VowelOrConsonant/Program.cs
+++ 05.ComplexConditionalStatements/07.VowelOrConsonant/Program.cs
@@ -7,30 +7,17 @@ namespace _07.VowelOrConsonant
         static void Main(string[] args)
         {
             //Input
-            string letter = Console.ReadLine();
+            char letter = char.Parse(Console.ReadLine());
             //Output
-            bool isValid = (letter == "B") || (letter == "C") || (letter == "D") || (letter == "F") || (letter == "G") ||
-             (letter == "H") || (letter == "J") || (letter == "K") || (letter == "L") || (letter == "M") ||
-             (letter == "N") || (letter == "P") || (letter == "Q") || (letter == "R") || (letter == "S") ||
-             (letter == "T") || (letter == "V") || (letter == "W") || (letter == "X") || (letter == "Y") ||
-             (letter == "Z") || (letter == "b") || (letter == "c") || (letter == "d") || (letter == "f") ||
-             (letter == "g") || (letter == "h") || (letter == "j") || (letter == "k") || (letter == "l") ||
-             (letter == "m") || (letter == "n") || (letter == "p") || (letter == "q") || (letter == "r") ||
-             (letter == "s") || (letter == "t") || (letter == "v") || (letter == "w") || (letter == "x") ||
-             (letter == "y") || (letter == "z");
-            bool isntValid = (letter == "A") || (letter == "E") || (letter == "I") || (letter == "O") || (letter == "U") ||
-         (letter == "a") || (letter == "e") || (letter == "i") || (letter == "o") || (letter == "u");
+            bool isValid = (letter == 'A') || (letter == 'E') || (letter == 'I') || (letter == 'O') || (letter == 'U') ||
+         (letter == 'a') || (letter == 'e') || (letter == 'i') || (letter == 'o') || (letter == 'u');
             if (isValid)
-            {
-                Console.WriteLine("Consonant");
-            }
-            else if (isntValid)
             {
                 Console.WriteLine("Vowel");
             }
             else
             {
-                Console.WriteLine("Error");
+                Console.WriteLine("Consonant");
             }
         }
     }
