5ca18e3bf6e9f884042a1b98a0b601c9392607a8
PBurgaski <pvalev95@gmail.com>
2025-01-15 06:17:14 +0200

    Update

--- 05.ComplexConditionalStatements/09.Cinema/Program.cs
+++ 05.ComplexConditionalStatements/09.Cinema/Program.cs
@@ -4,29 +4,28 @@
     {
         static void Main(string[] args)
         {
-            double premiere = 12.00;
-            double normal = 7.50;
-            double discount = 5.00;
             //Input
             string type = Console.ReadLine();
             int row = int.Parse(Console.ReadLine());
             int seat = int.Parse(Console.ReadLine());
+            //Calculations
+            double premiere = 12.00;
+            double normal = 7.50;
+            double discount = 5.00;
+            double count = row * seat;
             //Output
             if (type == "Premiere")
             {
-                double count = row * seat;
                 double total = count * premiere;
                 Console.WriteLine($"{total:F2}");
             }
             else if (type == "Normal")
             {
-                double count = row * seat;
                 double total = count * normal;
                 Console.WriteLine($"{total:F2}");
             }
             else if (type == "Discount")
             {
-                double count = row * seat;
                 double total = count * discount;
                 Console.WriteLine($"{total:F2}");
             }
